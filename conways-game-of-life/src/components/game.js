ad6d788a6869495ce9ed0cfc17086a95ff337296
Shawn McManus <xsvfan76@gmail.com>
2020-07-28 17:19:46 -0600

    functionality all working

--- conways-game-of-life/src/components/game.js
+++ conways-game-of-life/src/components/game.js
@@ -65,6 +65,7 @@ class Game extends React.Component {
   }
 
   handleClick = (event) => {
+    console.log("I'm being clicked!");
     const elemOffset = this.getElementOffset();
     const offsetX = event.clientX - elemOffset.x;
     const offsetY = event.clientY - elemOffset.y;
@@ -76,16 +77,94 @@ class Game extends React.Component {
     this.setState({ cells: this.makeCells() });
   };
 
-  runGame = () => {
+  runGame() {
     this.setState({ isRunning: true });
-  };
+    this.runIteration();
+  }
 
-  stopGame = () => {
+  stopGame() {
     this.setState({ isRunning: false });
-  };
+    if (this.timeoutHandler) {
+      window.clearTimeout(this.timeoutHandler);
+      this.timeoutHandler = null;
+    }
+  }
+
+  runIteration() {
+    let newBoard = this.makeEmptyBoard();
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y);
+        if (this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true;
+          } else {
+            newBoard[y][x] = false;
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true;
+          }
+        }
+      }
+    }
+
+    this.board = newBoard;
+    this.setState({ cells: this.makeCells() });
+    this.timeoutHandler = window.setTimeout(() => {
+      this.runIteration();
+    }, this.state.interval);
+  }
+
+  // Calculate the number of neighbors at point (x,y)
+  calculateNeighbors(board, x, y) {
+    let neighbors = 0;
+    const dirs = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+    ];
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = dirs[i];
+      let y1 = y + dir[0];
+      let x1 = x + dir[1];
+
+      if (
+        x1 >= 0 &&
+        x1 < this.cols &&
+        y1 >= 0 &&
+        y1 < this.rows &&
+        board[y1][x1]
+      ) {
+        neighbors++;
+      }
+    }
+    return neighbors;
+  }
 
   handleIntervalChange = (event) => {
     this.setState({ interval: event.target.value });
+  };
+
+  handleClear = () => {
+    this.board = this.makeEmptyBoard();
+    this.setState({ cells: this.makeCells() });
+  };
+
+  handleRandom = () => {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        this.board[y][x] = Math.random() >= 0.5;
+      }
+    }
+
+    this.setState({ cells: this.makeCells() });
   };
 
   render() {
@@ -99,7 +178,7 @@ class Game extends React.Component {
             height: HEIGHT,
             backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
           }}
-          onClick={this.handleClick}
+          onClick={(event) => this.handleClick(event)}
           ref={(n) => {
             this.boardRef = n;
           }}>
@@ -114,21 +193,21 @@ class Game extends React.Component {
             onChange={this.handleIntervalChange}
           />{' '}
           msec
-          {/* {isRunning ? (
-            <button className='button' onClick={this.stopGame}>
+          {this.state.isRunning ? (
+            <button className='button' onClick={() => this.stopGame()}>
               Stop
             </button>
           ) : (
-            <button className='button' onClick={this.runGame}>
+            <button className='button' onClick={() => this.runGame()}>
               Run
             </button>
-          )} */}
-          {/* <button className='button' onClick={this.handleRandom}>
+          )}
+          <button className='button' onClick={this.handleRandom}>
             Random
           </button>
           <button className='button' onClick={this.handleClear}>
             Clear
-          </button> */}
+          </button>
         </div>
       </div>
     );
